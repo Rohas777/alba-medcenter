df2f5089400d2ecb14e5709c3e9e742e9c2e79ab
Pislyakov Oleg <olegpislyakov@mail.ru>
2023-12-13 16:08:19 +0300

    legal-info page

--- js/script.js
+++ js/script.js
@@ -61,6 +61,40 @@ $(".specialists-swiper .swiper-button-next").click(function () {
 $(".specialists-swiper .swiper-button-prev").click(function () {
     swiper.slidePrev();
 });
+const legalPageTitles = [
+    "Правовая информация",
+    "Информация о методах оказания медицинской помощи",
+    "Лицензия",
+    "Перечень оказываемых платных услуг с ценами",
+    "Обращения (претензии)",
+    "Сроки ожидания предоставления услуг",
+];
+const info = $(".info").get();
+
+$(".info").hide();
+$(info[0]).show();
+
+$(".legal__nav-item").click(function () {
+    $(".legal__nav-item.active").removeClass("active");
+    $(this).addClass("active");
+    $(".hero__nav span").html(legalPageTitles[$(this).attr("id") - 1]);
+
+    $("html,body").animate(
+        {
+            scrollTop: $(".legal").offset().top,
+        },
+        "fast"
+    );
+
+    info.forEach((element) => {
+        if ($(element).attr("id") !== $(this).attr("id")) {
+            $(element).hide(300);
+        } else {
+            $(element).show(300);
+        }
+    });
+});
+
 
 /* accordeon */
 
